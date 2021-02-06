d7ded42d69a4d0bf4631c837269075531e5c71ec
Rafael Borges Lisboa <rafael5tag@hotmail.com>
2021-02-06 12:42:56 -0300

    Add the responsive menu side, icons and state animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import SideBar from './SideBar'
-import { IoMenu, IoPersonCircle, IoSearchOutline} from 'react-icons/io5'
+import { IoPersonCircle, IoSearchOutline } from 'react-icons/io5'
 import './styles/Header.css'
 
 const Header = () => {
     return (
-
         <header>
             <div className="logo">
                 <h3>MovieFlix</h3>
@@ -16,19 +15,19 @@ const Header = () => {
                         <li>Inicio</li>
                         <li>Filmes</li>
                         <li>Minha Lista</li>
-                        <li style={{paddingLeft: 60}}><i><IoPersonCircle /> </i></li>
+                        <li style={{ paddingLeft: 60 }}><i><IoPersonCircle /> </i></li>
                         <li><i><IoSearchOutline /> </i></li>
                     </ul>
 
                 </nav>
             </div>
             <div className="menu mobile">
-                <i><IoMenu /></i>
                 <SideBar />
             </div>
 
 
         </header>
     )
+
 }
 export default Header;
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { IoMenu, IoCloseSharp, IoHomeOutline, IoPlayOutline, IoListOutline } from 'react-icons/io5';
+import './styles/SideBar.css';
 
 const SideBar = () => {
+    let [state, setState] = useState(['close']);
+    let [icon, setIcon] = useState(['close']);
+
+    const changeState = () => {
+        if (state === 'open') {
+            setState('close');
+            setIcon('open');
+        } else {
+            setState('open');
+            setIcon('close');
+        }
+    }
+
     return (
-        
-        <nav class="nav-mobile">
-            <ul>
-                <li>Inicio</li>
-                <li>Filmes</li>
-                <li>Minha Lista</li>
-            </ul>
-        </nav>
+        <>
+            <div className="menu-icon" onClick={() => changeState()}>
+                {(icon === 'close') ? <IoCloseSharp id="isClose" /> :<IoMenu id="isOpen" /> }
+            </div>
+            <nav className={`nav-mobile-menu ${state}`}>
+                <ul>
+                    <li><i><IoHomeOutline /></i> Inicio</li>
+                    <li><i><IoPlayOutline /></i> Filmes</li>
+                    <li><i><IoListOutline /></i> Minha Lista</li>
+                </ul>
+            </nav>
+        </>
     )
+
 }
 
 export default SideBar;
