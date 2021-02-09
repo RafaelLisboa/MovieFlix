adc05c246598c8e1e3a59d2623197bef059c4245
Rafael Borges Lisboa <rafael5tag@hotmail.com>
2021-02-09 20:01:58 -0300

    fix

--- src/movies.js
+++ src/movies.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 
 const api_key = process.env.REACT_APP_API_KEY;
 const urlBase = "https://api.themoviedb.org/3/discover/";
-// https://api.themoviedb.org/3/?api_key=e29e9dbf058dc6c797f9538fa41e8844
-
 const getContentData = async(genrerID) => {
     const url = `${urlBase}movie/?api_key=${api_key}&with_genres=${genrerID}&language=pt-BR`;
     let fecthData = await fetch(url);
